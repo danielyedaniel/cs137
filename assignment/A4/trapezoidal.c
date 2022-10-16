666543254cb9edbc9ba70540fd0416975b5a2082
Daniel <yedaniel99@gmail.com>
2022-10-16 09:40:21 -0400

    adding to a4

---
+++ assignment/A4/trapezoidal.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+#define PI acos(-1)
+
+double trapezoidal(double (*f)(double), double a, double b, double epsilon, int n){
+    
+}
+
+double f1(double x)
+{
+    return x * x;
+}
+
+double f2(double x)
+{
+    return cos(x);
+}
+
+double f3(double x)
+{
+    return sqrt(x);
+}
+
+double trapezoidal(double (*f)(double), double a, double b, double epsilon, int n)
+{
+}
+
+int main(void)
+{
+    assert(fabs(trapezoidal(f1, 5, 10, 0.00001, 2) - 291.667) <= 0.001);
+    assert(fabs(trapezoidal(f2, PI / 2, 3, 0.00001, 5) - -0.858879) <=
+           00001);
+    assert(fabs(trapezoidal(f3, 1, 4, 0.00001, 2) - 4.666) <= 0.001);
+    return 0;
+}
