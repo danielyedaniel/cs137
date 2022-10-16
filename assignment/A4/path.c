d41cfb46dec7723927dc75bfd03fa0a3bfac3357
Daniel <yedaniel99@gmail.com>
2022-10-16 10:02:04 -0400

    final path.c solution

--- assignment/A4/path.c
+++ assignment/A4/path.c
@@ -1,52 +1,16 @@
 #include <stdio.h>
 #include <assert.h>
 
-int monotonePath(int m, int n, int a[m][n]){ 
-    // printf("%d , %d , %d \n", a[m-1][n-1], m, n);
-    printf("%d %d %d\n\n", a[9][10], m, n);
-    for(int x=0;x<m;x++){
-        for(int i=0;i<n;i++){
-            printf("%d ",a[x][i]);
-        }
-        printf("\n");
-    }
-    if(a[m-1][n-1]==0)return 0;
-    if(m-1==0 && n-1==0)return 1;
-    if(n>0)if(monotonePath(m,n-1,a))return 1;
-    if(m>0)if(monotonePath(m-1,n,a))return 1;
+int help(int i, int j, int m, int n, int a[m][n]){ 
+    if(a[m-i-1][n-j-1]==0)return 0;
+    if(m-i-1==0 && n-j-1==0)return 1;
+    if(n-j>0)if(help(i,j+1,m,n,a))return 1;
+    if(m-i>0)if(help(i+1,j,m,n,a))return 1;
     return 0;
 }
-int main(void)
-{
-    int m = 10;
-    int n = 12;
-    int a[][12] = {
-    {1,1,0,0,0,0,0,0,0,0,0,0},
-    {0,1,1,0,0,0,0,0,0,0,0,0},
-    {0,0,1,1,1,1,0,0,0,0,0,0},
-    {0,0,1,0,0,1,0,0,0,0,0,0},
-    {0,1,1,0,0,1,1,0,0,0,0,0},
-    {0,1,0,0,0,0,1,0,0,0,0,0},
-    {0,1,1,0,0,0,1,1,1,0,0,0},
-    {0,0,1,0,0,0,0,0,1,0,0,0},
-    {0,0,1,1,1,1,1,1,1,1,1,0},
-    {0,0,0,0,0,0,0,0,0,0,1,1}
-    };
 
-    // a, but with one entry switched eliminating the monotone path
-    int b[10][12] = {{1,1,0,0,0,0,0,0,0,0,0,0},
-    {0,1,1,0,0,0,0,0,0,0,0,0},
-    {0,0,1,1,0,1,0,0,0,0,0,0},
-    {0,0,1,0,0,1,0,0,0,0,0,0},
-    {0,1,1,0,0,1,1,0,0,0,0,0},
-    {0,1,0,0,0,0,1,0,0,0,0,0},
-    {0,1,1,0,0,0,1,1,1,0,0,0},
-    {0,0,1,0,0,0,0,0,1,0,0,0},
-    {0,0,1,1,1,1,1,1,1,1,1,0},
-    {0,0,0,0,0,0,0,0,0,0,1,1}
-    };
-
-    printf("%d",monotonePath(m, n, a));
-    // assert (!monotonePath(m, n, b));
-    return 0;
- }
+int monotonePath(int m, int n, int a[m][n]){ 
+    int i=0;
+    int j=0;
+    return help(i,j,m,n,a);
+}
