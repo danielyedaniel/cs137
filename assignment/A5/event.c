7a36e0c9d756c1271646044194c2d6659f311900
Daniel <yedaniel99@gmail.com>
2022-11-04 21:38:35 -0400

    custom test cases to test code

--- assignment/A5/event.c
+++ assignment/A5/event.c
@@ -24,3 +24,45 @@ int freetime (struct event schedule[], int n, int hour, int min){
     return 1;
     
 }
+
+int main (void)
+{
+ struct event schedule[] = {{{9,45},{9,55}},{{13,0},
+ {14,20}},{{15,20},{16,30}}, {{17,0}, {19, 0}}};
+
+
+assert(freetime(schedule, 4, 9, 30));
+assert(freetime(schedule, 4, 9, 57));
+assert(!freetime(schedule, 4, 9, 50));
+assert(!freetime(schedule, 4, 9, 45));
+assert(freetime(schedule, 4, 9, 55));
+
+assert(freetime(schedule, 4, 4, 0));
+assert(freetime(schedule, 4, 9, 0));
+assert(freetime(schedule, 4, 10, 0));
+assert(freetime(schedule, 4, 23, 0));
+
+assert(!freetime(schedule, 4, 13, 0));
+assert(!freetime(schedule, 4, 13, 34));
+assert(!freetime(schedule, 4, 14, 0));
+assert(!freetime(schedule, 4, 14, 10));
+assert(freetime(schedule, 4, 14, 20));
+assert(freetime(schedule, 4, 14, 50));
+
+assert(freetime(schedule, 4, 15, 0));
+assert(!freetime(schedule, 4, 15, 20));
+assert(!freetime(schedule, 4, 15, 21));
+assert(!freetime(schedule, 4, 16, 0));
+assert(!freetime(schedule, 4, 16, 29));
+assert(freetime(schedule, 4, 16, 30));
+assert(freetime(schedule, 4, 16, 40));
+
+assert(!freetime(schedule, 4, 17, 0));
+assert(!freetime(schedule, 4, 17, 30));
+assert(!freetime(schedule, 4, 18, 0));
+assert(!freetime(schedule, 4, 18, 40));
+assert(freetime(schedule, 4, 19, 0));
+
+
+ return 0;
+ }
