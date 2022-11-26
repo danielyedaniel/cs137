1e67bb9efb3ffec3c09cbae82ff06cb3792f4445
Daniel <yedaniel99@gmail.com>
2022-11-26 10:23:06 -0500

    updated sort.c

--- assignment/A8/sort.c
+++ assignment/A8/sort.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
-
-struct point
-{
-    int x;
-    int y;
-};
 void sort(void *arr, int n, size_t elemsize, bool (*compare)(const void *a, const void *b), void (*swap)(void *a, void *b)){
-    for(int i=1;i<n;i++){
-        struct point *temp=((char *) arr) + i * elemsize;
-        int x=temp->x;
-        int y=temp->y;
-        struct point tem={x,y};
-        struct point *a=&tem;
-        int count=i-1;
-        while(count>=0 && compare(a,((char *) arr) + count * elemsize)){ 
-            swap(((char *) arr) + count * elemsize+elemsize,((char *) arr) + count * elemsize);
-            count--;
+    for(int i=0;i<n;i++){
+        bool sw=false;
+        for(int j=0;j<n-1;j++){
+            if(compare(((char *) arr) + (j+1) * elemsize,((char *) arr) + j * elemsize)){
+                swap(((char *) arr) + (j+1) * elemsize,((char *) arr) + j * elemsize);
+                sw=true;
+            }
         }
-        struct point *p=((char *) arr) + (count+1) * elemsize;
-        p->x=x;
-        p->y=y;
+        if(!sw)break;
     }
 }
