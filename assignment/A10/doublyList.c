6ab25dc4d7308397b7f2ed8e75c3bc9876266c22
Daniel <yedaniel99@gmail.com>
2022-12-06 21:19:10 -0500

    fixing solution

--- assignment/A10/doublyList.c
+++ assignment/A10/doublyList.c
@@ -16,6 +16,25 @@ typedef struct {
 	int len;	// number of elements
 } ndlst;
 
+void dlistPrint(ndlst *lst)
+{
+	dlnode *node = lst->head;
+	// iterate over the nodes (items) until node is NULL 
+	// and print the data
+	for (; node; node = node->next)
+		printf("%d  ", node->data);
+	printf("\n");
+}
+
+void dlistPrintReverse(ndlst *lst)
+{
+	dlnode *node = lst->tail;
+	// iterate over the nodes (items) until node is NULL 
+	// and print the data
+	for (; node; node = node->prev)
+		printf("%d  ", node->data);
+	printf("\n");
+}
 //Pre: None
 //Post: Creates a null list
 struct ndlst *dlistCreate(void)
@@ -154,12 +173,13 @@ void dlistSort(ndlst *lst)
 // rotate the list n times to the left, assume, lst not empty
 void dlistRotateLeft(ndlst *lst, int n)
 {
-	if(lst->len>=1){
+	if(lst->len>1){
 		int c=0;
 		while(c<n){
 			struct dlnode * h=lst->head;
 			struct dlnode * e=lst->tail;
 			lst->head=h->next;
+			h->next->prev=NULL;
 			lst->tail=h;
 			e->next=h;
 			h->prev=e;
@@ -172,7 +192,7 @@ void dlistRotateLeft(ndlst *lst, int n)
 // rotate the list n times to the right, assume, lst not empty
 void dlistRotateRight(ndlst *lst, int n)
 {
-	if(lst->len>=1){
+	if(lst->len>1){
 		int c=0;
 		while(c<n){
 			struct dlnode * h=lst->head;
@@ -188,3 +208,36 @@ void dlistRotateRight(ndlst *lst, int n)
 	}
 }
 
+int main(void)
+{
+ndlst *lst1 = dlistCreate();
+assert(dlistLength(lst1) == lst1->len);
+assert(dlistLength(lst1) == 0);
+dlistAddToEnd(lst1, 10);
+dlistAddToFront(lst1, -20);
+dlistAddToFront(lst1, 0);
+dlistAddToEnd(lst1, 9);
+dlistAddToFront(lst1, -9);
+dlistAddToFront(lst1, 7);
+dlistAddToEnd(lst1, 40);
+assert(dlistLength(lst1) == lst1->len);
+assert(dlistLength(lst1) == 7);
+dlistPrint(lst1);
+dlistPrintReverse(lst1);
+dlistRemoveElem(lst1, 5);
+dlistRemoveElem(lst1, 1);
+dlistRemoveElem(lst1, 5);
+dlistAddToEnd(lst1, 100);
+assert(dlistLength(lst1) == lst1->len);
+assert(dlistLength(lst1) == 5);
+dlistPrint(lst1);
+dlistPrintReverse(lst1);
+dlistSort(lst1);
+dlistPrint(lst1);
+dlistRotateLeft(lst1, 2);
+dlistPrint(lst1);
+dlistRotateRight(lst1, 3);
+dlistPrint(lst1);
+dlistDestroy(lst1);
+return 0;
+}
