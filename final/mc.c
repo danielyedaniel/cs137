3425c32cf5eef4c7ed99e76d604704e0f80bab44
Daniel <yedaniel99@gmail.com>
2022-12-14 23:29:22 -0500

    practice before the final

--- final/mc.c
+++ final/mc.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+int binarySearch(int *a,int left, int right, int target){
+    if(left>right)return -1;
+    int middle=(right+left)/2;
+    if(a[middle]==target)return middle;
+    if(a[middle]<target)return binarySearch(a,middle+1,right,target);
+    return binarySearch(a,left,middle-1,target);
+}
+
 int main(void){
-    char c = 1;
-    c &= 7;
-    c |= 5;
-    c *= 9;
-    printf("%d",c);
+    int a[]={0,1,2,3,4,5,6,7,8,9,10};
+    printf("%d\n",binarySearch(a,0,10,7));
 }
---
+++ final/practice.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){
+    char d;
+    scanf("%c",&d);
+    printf("%d",d);
+}
